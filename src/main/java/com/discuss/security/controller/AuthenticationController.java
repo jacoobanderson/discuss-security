536cb46cdb7de99eb926f9288069bcf136060fc5
jacoobanderson <j00cwa@gmail.com>
2023-12-29 03:12:05 +0100

    implement controller authentication methods

--- src/main/java/com/discuss/security/controller/AuthenticationController.java
+++ src/main/java/com/discuss/security/controller/AuthenticationController.java
@@ -1,14 +1,19 @@
 package com.discuss.security.controller;
 
+import com.discuss.security.model.AuthenticationRequest;
 import com.discuss.security.model.AuthenticationResponse;
 import com.discuss.security.model.RegisterRequest;
 import com.discuss.security.service.AuthenticationService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -21,4 +26,13 @@ public class AuthenticationController {
         return ResponseEntity.ok(authenticationService.register(request));
     }
 
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+    @PostMapping("/refresh-token")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        authenticationService.refreshToken(request, response);
+    }
 }
